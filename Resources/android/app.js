5296b91a70128dafe3c966c4f52a54c026175bd2
ianthomas0 <ianthomas0@live.com>
2014-07-24 22:58:12 -0400

    Remove resource dir

--- Resources/android/app.js
+++
@@ -1,7 +0,0 @@
-var Alloy = require("alloy"), _ = Alloy._, Backbone = Alloy.Backbone;
-
-Alloy.Globals.Map = require("ti.map");
-
-Alloy.Globals.Cloud = require("ti.cloud");
-
-Alloy.createController("index");
